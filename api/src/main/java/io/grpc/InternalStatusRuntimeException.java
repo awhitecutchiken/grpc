c3b4984c6b5ac696affc53298228c3aac931470d
Alex Panchenko <alex.panchenko@gmail.com>
2024-04-04 13:55:18 +0200

    synchronized

--- api/src/main/java/io/grpc/InternalStatusRuntimeException.java
+++ api/src/main/java/io/grpc/InternalStatusRuntimeException.java
@@ -10,7 +10,7 @@ class InternalStatusRuntimeException extends StatusRuntimeException {
   }
 
   @Override
-  public Throwable fillInStackTrace() {
+  public synchronized Throwable fillInStackTrace() {
     return this;
   }
 }
