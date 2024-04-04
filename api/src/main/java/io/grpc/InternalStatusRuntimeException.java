aa0dc0f179dca3f4bac01a296c6a05e67635328e
Alex Panchenko <alex.panchenko@gmail.com>
2024-04-04 13:52:36 +0200

    serialVersionUID

--- api/src/main/java/io/grpc/InternalStatusRuntimeException.java
+++ api/src/main/java/io/grpc/InternalStatusRuntimeException.java
@@ -3,6 +3,8 @@ package io.grpc;
 import javax.annotation.Nullable;
 
 class InternalStatusRuntimeException extends StatusRuntimeException {
+  private static final long serialVersionUID = 5225396310618305864L;
+
   public InternalStatusRuntimeException(Status status, @Nullable Metadata trailers) {
     super(status, trailers);
   }
